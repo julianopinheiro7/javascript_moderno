be2007cf93f4662a433928e9d974259fe464f981
julianopinheiro7 <julianopinheiro.adm@gmail.com>
2018-03-12 15:16:33 -0300

    Função
    Conceitos de Função

---
+++ exercicios-js/fundamentos/funcao1.js
@@ -0,0 +1,18 @@
+// Funcão sem retorno
+function imprimirSoma(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2) //nesse caso o segundo parametro fica como undefined
+imprimirSoma(2, 10, 4, 5, 6, 7, 8) //no caso ele ignora os demais dados
+imprimirSoma()
+
+//Função com retorno
+function soma(a, b = 1) {
+    return a + b
+}
+
+console.log(soma(2,3))
+console.log(soma(2))
+console.log(soma())
