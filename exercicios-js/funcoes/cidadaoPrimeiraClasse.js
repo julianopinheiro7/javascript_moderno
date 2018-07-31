303521e32648dc7951f667f036c178c7c4743029
julianopinheiro7 <julianopinheiro.adm@gmail.com>
2018-07-31 13:58:37 -0300

    Função
    Inicia da parte de Função

---
+++ exercicios-js/funcoes/cidadaoPrimeiraClasse.js
@@ -0,0 +1,14 @@
+// Função em JS é First-Class Object (Citizens)
+// Higher-order function 
+
+//criar de forma literal
+function fun1() {
+
+}
+
+//Armazenar em uma variavel
+const fun2 = function () { }
+
+//Armazenar em um array
+const array = [function (a,b) {return a + b}]
+
