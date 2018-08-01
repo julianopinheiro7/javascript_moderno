7fe4f7bc5abd706a3fdb6bfe966763df28118542
Juliano Pinheiro <julianopinheiro.adm@gmail.com>
2018-07-31 22:22:39 -0300

    Funções 1
    Funções 1

--- exercicios-js/funcoes/cidadaoPrimeiraClasse.js
+++ exercicios-js/funcoes/cidadaoPrimeiraClasse.js
@@ -10,5 +10,28 @@ function fun1() {
 const fun2 = function () { }
 
 //Armazenar em um array
-const array = [function (a,b) {return a + b}]
+const array = [function (a,b) {return a + b}, fun1, fun2]
 
+//Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function () {return 'Opa'}
+console.log(obj.falar())
+
+//Passar função como parametro
+
+function run(fun) {
+    fun()
+}
+
+run( function () {console.log('Executando...')})
+
+// Uma função pode retornar/conter uma função.
+function soma(a, b) {
+    return function (c) {
+        console.log(a + b + c)
+    }
+}
+
+soma(3, 7)(2)
+const dez = soma(2,3)
+dez(2)
