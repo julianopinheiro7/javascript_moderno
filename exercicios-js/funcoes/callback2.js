73e2c4b26f466ef18f5f0a524517e82e90c72230
julianopinheiro7 <julianopinheiro.adm@gmail.com>
2018-08-09 14:49:56 -0300

    CallBack 2
    Estrutura de Callback

--- exercicios-js/funcoes/callback2.js
+++ exercicios-js/funcoes/callback2.js
@@ -1,1 +1,24 @@
 const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+//Sem callback
+const notasBaixas1 = []
+for (let i in notas) {
+    if (notas[i] < 7) {
+        notasBaixas1.push(notas[i])
+    }
+}
+console.log(notasBaixas1)
+
+//Com Callback
+const notasBaixas2 = notas.filter(function (nota) {
+    return nota < 7
+})
+console.log(notasBaixas2)
+
+//Com Callback e arrow
+const notasMenorque7 = nota => nota < 7
+const notasBaixas3 = notas.filter(notasMenorque7)
+console.log(notasBaixas3)
+
+
+
