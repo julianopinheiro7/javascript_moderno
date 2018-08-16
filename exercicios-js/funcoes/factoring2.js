e966e6746b6fbc7e1f026051191815a2cc98b351
julianopinheiro7 <julianopinheiro.adm@gmail.com>
2018-08-16 13:53:55 -0300

    Conceitos de Função
    CallApply, Factoring e iife

---
+++ exercicios-js/funcoes/factoring2.js
@@ -0,0 +1,10 @@
+//Factoring com parametros:
+function criarProduto(nome, preco) {
+    return {
+        nome,
+        preco,
+        desconto: 0.1
+    }   
+}
+
+console.log(criarProduto('Sabonete', 1.99))
